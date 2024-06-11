809be18ab619e2b5f6ee6bb36a1e0e7e1e7eb579
feuzll <139976001+feuzll@users.noreply.github.com>
2024-06-11 19:27:37 +0700

    messy, but success of webm export

--- thwebgpu.js
+++ thwebgpu.js
@@ -7,23 +7,50 @@ import { toneMapping, color, viewportTopLeft } from "three/nodes";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 800 / 600, 0.1, 1000);
 
-scene.background = new THREE.Color(0xa0a0a0);
+// scene.background = new THREE.Color(0xa0a0a0);
+//scene.background = new THREE.Color(0xff0000);
 scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
 
 // const renderer = new THREE.WebGLRenderer();
 
-
-if ( WebGPU.isAvailable() === false) {
-    throw new Error( 'WebGPU not available' );
+if (WebGPU.isAvailable() === false) {
+  throw new Error("WebGPU not available");
 }
-const renderer = new WebGPURenderer();
+const renderer = new WebGPURenderer({alpha: true});
 renderer.toneMappingNode = toneMapping(THREE.LinearToneMapping, 0.4);
-
 
 renderer.setSize(800, 600);
 renderer.setAnimationLoop(animate);
 renderer.shadowMap.enabled = true;
-document.body.appendChild(renderer.domElement);
+const insideDoc = document.body.appendChild(renderer.domElement);
+
+let mediaRecorder = null
+
+function recordIntoWebm() {
+  // setup recording
+  const canvasElt = insideDoc;
+  // Get the stream
+  const stream = canvasElt.captureStream(25); // 25 FPS
+  mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.start();
+  mediaRecorder.addEventListener("dataavailable", finishCapturing);
+
+  function finishCapturing(e) {
+    //capturing = false;
+    var videoData = [e.data];
+    var blob = new Blob(videoData, { type: "video/webm" });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = "test.webm";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+}
+
+recordIntoWebm();
 
 ////lights
 
@@ -57,33 +84,57 @@ scene.add(ambient);
 // const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
-camera.position.z = 3;
-camera.position.y = 1;
+camera.position.z = 1;
+camera.position.y = 1.5;
+
+const clock = new THREE.Clock();
+
+function createFloor() {
+  const mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(100, 100),
+    new THREE.MeshToonMaterial({ color: 0xcbcbcb, depthWrite: false })
+  );
+  mesh.rotation.x = -Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add(mesh);
+}
+
+function createTorus() {
+  const torusGeometry = new THREE.TorusKnotGeometry(1, 0.4, 100, 16);
+  const torusMaterial = new THREE.MeshToonMaterial({ color: 0xffff00 });
+  const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+  torus.receiveShadow = true;
+  scene.add(torus);
+  torus.position.x = 2;
+}
 
 const loader = new GLTFLoader();
 let mixer = null;
-const clock = new THREE.Clock();
-
-const mesh = new THREE.Mesh(
-  new THREE.PlaneGeometry(100, 100),
-  new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false })
-);
-mesh.rotation.x = -Math.PI / 2;
-mesh.receiveShadow = true;
-scene.add(mesh);
-
 loader.load(
-  "target.glb",
+  "chloe.glb",
   function (gltf) {
     scene.add(gltf.scene);
+    gltf.scene.rotation.y = THREE.MathUtils.degToRad(10);
     gltf.scene.traverse(function (object) {
-      if (object.isMesh) object.castShadow = true;
+      if (object.isMesh) {
+        console.log(object.material);
+        let lastTexture = object.material.map;
+        object.material = new THREE.MeshToonMaterial();
+        object.material.map = lastTexture;
+        object.receiveShadow = true;
+        object.castShadow = true;
+      }
     });
 
     let mesh = gltf.scene;
     mixer = new THREE.AnimationMixer(mesh);
-    const clip = gltf.animations[0];
+    const clip = gltf.animations[2];
     const action = mixer.clipAction(clip);
+    action.loop = THREE.LoopOnce;
+    mixer.addEventListener("finished", function (e) {
+      console.log("animation ended");
+      mediaRecorder.stop();
+    });
     action.play();
   },
   undefined,
@@ -91,6 +142,12 @@ loader.load(
     console.error(error);
   }
 );
+
+document.addEventListener("keypress", function onEvent(event) {
+  if (event.key == "t") {
+    console.log("Pressed t!");
+  }
+});
 
 function animate() {
   // cube.rotation.x += 0.01;
